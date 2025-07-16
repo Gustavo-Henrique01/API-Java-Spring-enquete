a4615a209af24c212b84f489dc7dac25fdbfee90
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2025-07-16 14:38:58 -0400

    ajuste no GlobalExeption

--- sistemaenquetes/src/main/java/enquetes/sistemaenquetes/exception/GlobalExceptionHandler.java
+++ sistemaenquetes/src/main/java/enquetes/sistemaenquetes/exception/GlobalExceptionHandler.java
@@ -1,74 +1,93 @@
 package enquetes.sistemaenquetes.exception;
 
-
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseStatus; 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException; 
 
-@RestControllerAdvice
+@RestControllerAdvice 
 public class GlobalExceptionHandler {
-	
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> tratamentoExcecaoValidacao(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String atributo = ((FieldError) error).getField();
-			String mensagem = error.getDefaultMessage();
-			errors.put(atributo, mensagem);
-		});
-		return errors;
-	}
-	
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public Map<String, String> ResourceNotFoundException (ResourceNotFoundException ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(UserAlreadyVotedException.class)
-	public Map<String, String> UserAlreadyVotedException (UserAlreadyVotedException  ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(InvalidPollDateException.class)
-	public Map<String, String> InvalidPollDateException (InvalidPollDateException  ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(PollNotActiveException.class)
-	public Map<String, String> PollNotActiveException (PollNotActiveException  ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
-	
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(UsernameAlreadyExistsException.class)
-	public Map<String, String> UsernameAlreadyExistsException (UsernameAlreadyExistsException  ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
-	
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(UsernameNotFoundException.class)
-	public Map<String, String> UsernameNotFoundException (UsernameNotFoundException  ex){
-		Map<String, String> errosMap = new HashMap<String, String>();
-		errosMap.put("mensagem", ex.getMessage());
-		return errosMap;
-	}
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST) 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorsMap, HttpStatus.NOT_FOUND); 
+    }
+
+ 
+    @ExceptionHandler(UserAlreadyVotedException.class)
+    public ResponseEntity<Map<String, String>> handleUserAlreadyVotedException(UserAlreadyVotedException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorsMap, HttpStatus.CONFLICT); 
+    }
+
+    @ExceptionHandler(InvalidPollDateException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidPollDateException(InvalidPollDateException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST); 
+    }
+
+    @ExceptionHandler(PollNotActiveException.class)
+    public ResponseEntity<Map<String, String>> handlePollNotActiveException(PollNotActiveException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST); 
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorsMap, HttpStatus.CONFLICT); 
+    }
+    
+  
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", ex.getMessage());
+      
+        return new ResponseEntity<>(errorsMap, HttpStatus.NOT_FOUND); 
+    }
+
+   
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDeniedException(AccessDeniedException ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", "Você não tem permissão para acessar este recurso.");
+        return new ResponseEntity<>(errorsMap, HttpStatus.FORBIDDEN); 
+    }
+
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("message", "Ocorreu um erro interno no servidor.");
+       
+        ex.printStackTrace(); 
+        return new ResponseEntity<>(errorsMap, HttpStatus.INTERNAL_SERVER_ERROR); 
+    }
 }
